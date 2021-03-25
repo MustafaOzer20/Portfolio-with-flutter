7c93cfdef4aad9b02193aa7cee0de67f58dab563
MustafaOzer20 <ozerstt@hotmail.com>
2021-03-25 15:33:43 +0300

    overflow fixed in mobile tools screen

--- lib/widgets/tablet_widgets/tools_row.dart
+++ lib/widgets/tablet_widgets/tools_row.dart
@@ -4,16 +4,17 @@ import 'package:portfolio/widgets/tools_images.dart';
 class ToolsRow extends StatelessWidget {
   final String imageUrl1;
   final String imageUrl2;
+  final double size;
 
-  const ToolsRow({@required this.imageUrl1, @required this.imageUrl2});
+  const ToolsRow({@required this.imageUrl1, @required this.imageUrl2, this.size = 200});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ToolImage(imageUrl: imageUrl1,width: 200,height: 200,),
-        ToolImage(imageUrl: imageUrl2,width: 200,height: 200,),
+        ToolImage(imageUrl: imageUrl1,width: size,height: size,),
+        ToolImage(imageUrl: imageUrl2,width: size,height: size,),
       ],
     );
   }
